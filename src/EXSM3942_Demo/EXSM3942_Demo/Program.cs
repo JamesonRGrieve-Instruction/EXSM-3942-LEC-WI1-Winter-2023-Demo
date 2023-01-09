ae3f16019d3d096e18dce61088adf954ffb59178
James Grieve <jgrieve@ualberta.ca>
2023-01-09 12:29:03 -0700

    Initialized new console application;

---
+++ src/EXSM3942_Demo/EXSM3942_Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EXSM3942_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
