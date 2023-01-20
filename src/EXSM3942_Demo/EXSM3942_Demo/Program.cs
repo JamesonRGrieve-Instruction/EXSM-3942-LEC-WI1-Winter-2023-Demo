23a8fbdfae7a72c423757aaab0bbcac55d1c85ee
James Grieve <jgrieve@ualberta.ca>
2023-01-19 19:18:36 -0700

    Script solution;

--- src/EXSM3942_Demo/EXSM3942_Demo/Program.cs
+++ src/EXSM3942_Demo/EXSM3942_Demo/Program.cs
@@ -4,18 +4,64 @@
     {
         static void Main(string[] args)
         {
-            Pen myPen = new Pen("Bic", "Black", 55.2m);
             try
             {
-                myPen.Write(100);
-                myPen.Write(42);
-                myPen.Write(200);
+                Console.Write("Please select your utensil.\n1) Pen\n2) Pencil\n\tChoose: ");
+                int choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    Pen pen = new Pen("Bic", "Black");
+                    string userEntry = "";
+                    do
+                    {
+                        Console.Write("Enter a number of letters to write, or 'exit' to quit: ");
+                        userEntry = Console.ReadLine();
+                        if (userEntry != "exit")
+                        {
+                            try
+                            {
+                                pen.Write(int.Parse(userEntry));
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+                            Console.WriteLine($"Pen has {pen.InkLevel}% ink remaining.");
+                        }
+                    } while (userEntry != "exit");
+                }
+                else if (choice == 2)
+                {
+                    Pencil pencil = new Pencil("Dixon");
+                    string userEntry = "";
+                    do
+                    {
+                        Console.Write("Enter a number of letters to write, or 'exit' to quit: ");
+                        userEntry = Console.ReadLine();
+                        if (userEntry != "exit")
+                        {
+                            try
+                            {
+                                pencil.Write(int.Parse(userEntry));
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+                            Console.WriteLine($"Pencil has {pencil.Length}% length remaining.");
+                        }
+                    } while (userEntry != "exit");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid entry. Ending program.");
+                }
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("Error, Aborting Program");
+                Console.WriteLine(e.Message);
             }
-            Console.WriteLine(myPen.SummarizeInkLevel());
         }
     }
 }
