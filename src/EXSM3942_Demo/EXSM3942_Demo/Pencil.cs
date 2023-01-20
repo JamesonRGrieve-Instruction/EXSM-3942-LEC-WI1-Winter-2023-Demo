e994e0406442faa558b8186abc9c9e4ecfbfc20a
James Grieve <jgrieve@ualberta.ca>
2023-01-19 19:11:25 -0700

    Classes built;

--- src/EXSM3942_Demo/EXSM3942_Demo/Pencil.cs
+++ src/EXSM3942_Demo/EXSM3942_Demo/Pencil.cs
@@ -6,36 +6,31 @@ using System.Threading.Tasks;
 
 namespace EXSM3942_Demo
 {
-    internal class Pen : WritingUtensil
+    internal class Pencil : WritingUtensil
     {
-        public Pen(string brand, string colour)
+        public Pencil(string brand)
         {
             Brand = brand;
-            Colour = colour;
         }
-
-
-        public string Colour { get; set; }
-        private double _inkLevel = 100;
-        public double InkLevel
-        {
-            get => _inkLevel; 
+        private double _length = 100;
+        public double Length {
+            get => _length;
             set
             {
                 if (value < 0)
                 {
-                    throw new Exception("The pen has insufficient ink!");
+                    throw new Exception("The pencil has insufficient length!");
                 }
                 else
                 {
-                    _inkLevel = value;
+                    _length = value;
                 }
             }
 
         }
         public override void Write(int letterCount)
         {
-            InkLevel -= letterCount * 0.5;
+            Length -= letterCount * 0.25;
         }
     }
 }
---
+++ src/EXSM3942_Demo/EXSM3942_Demo/WritingUtensil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXSM3942_Demo
+{
+    internal abstract class WritingUtensil
+    {
+        public string Brand { get; set; }
+        public abstract void Write(int letterCount);
+    }
+}
