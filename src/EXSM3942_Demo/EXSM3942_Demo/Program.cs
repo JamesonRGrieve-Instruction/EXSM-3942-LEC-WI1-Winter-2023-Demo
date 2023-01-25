623ecf717c2901a0f4b2288dd692c43262eeea21
James Grieve <jgrieve@ualberta.ca>
2023-01-24 19:36:40 -0700

    Basic Windows hooks;

--- src/EXSM3942_Demo/EXSM3942_Demo/Program.cs
+++ src/EXSM3942_Demo/EXSM3942_Demo/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace EXSM3942_Demo
+﻿using System.IO;
+
+namespace EXSM3942_Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // We can use Environment to access some information to address the user and their machine.
+            Console.WriteLine($"Hello, {Environment.UserName} of {Environment.MachineName} running {Environment.Version}!");
+            // We can also access a variety of configured "special" directories so we can store files in a common easy-to-find location.
+            Console.WriteLine($"Your system directory is: {Environment.SystemDirectory}");
+            Console.WriteLine($"Your temp directory is: {Environment.GetEnvironmentVariable("TEMP")}");
+            Console.WriteLine($"Your Documents directory is: {Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}");
+
+            // We can show all the files in a directory:
+            foreach(string filename in Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Desktop))) Console.WriteLine(filename);
         }
     }
 }
