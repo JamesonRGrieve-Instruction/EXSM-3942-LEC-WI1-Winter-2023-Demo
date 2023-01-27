5415816f08cb5b3af7cd5f63c823ff823829474c
James Grieve <jgrieve@ualberta.ca>
2023-01-26 19:31:20 -0700

    API requests;

--- src/EXSM3942_Demo/EXSM3942_Demo/Program.cs
+++ src/EXSM3942_Demo/EXSM3942_Demo/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Text;
 
 namespace EXSM3942_Demo
 {
@@ -6,6 +8,58 @@ namespace EXSM3942_Demo
     {
         static void Main(string[] args)
         {
+            AsyncExample();
+
+            Console.ReadLine();
+        }
+        static async void AsyncExample()
+        {
+            {
+                // ----
+                // All Info
+                // ----
+                // Generate a client to handle our HTTP request.
+                HttpClient client = new HttpClient();
+                // Houses the response object for the request.
+                HttpResponseMessage response;
+                // URL for our request.
+                string destination = "http://numbersapi.com/random/math";
+                // Send the request and wait for a response.
+                response = await client.GetAsync(destination);
+                // Process the response into a byte array.
+                byte[] responseContent = await response.Content.ReadAsByteArrayAsync();
+                // Encode the byte array into a string using the default encoding system.
+                string responseString = Encoding.Default.GetString(responseContent);
+                // Display the response string.
+                Console.WriteLine(responseString);
+            }
+            {
+                // ----
+                // String Result
+                // ----
+                // Generate a client to handle our HTTP request.
+                HttpClient client = new HttpClient();
+                // URL for our request.
+                string destination = "http://numbersapi.com/random/math";
+                // Send the request and wait for a response.
+                string responseString = await client.GetStringAsync(destination);
+                // Display the response string.
+                Console.WriteLine(responseString);
+            }
+            {
+                // ----
+                // JSON Result
+                // ----
+                // Generate a client to handle our HTTP request.
+                HttpClient client = new HttpClient();
+                // URL for our request.
+                string destination = "https://pokeapi.co/api/v2/pokemon/pikachu";
+                // Send the request and wait for a response.
+                string responseString = await client.GetStringAsync(destination);
+                // Process the response into JSON.
+                Pokemon myDeserializedClass = JsonConvert.DeserializeObject<Pokemon>(responseString);
+                Console.WriteLine(myDeserializedClass.Types[0].Type.Name);
+            }
 
         }
     }
