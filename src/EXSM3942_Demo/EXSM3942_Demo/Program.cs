2a86970205b8c2015925cb3c0bd00483edb04141
James Grieve <jgrieve@ualberta.ca>
2023-01-31 19:10:01 -0700

    Solution;

--- src/EXSM3942_Demo/EXSM3942_Demo/Program.cs
+++ src/EXSM3942_Demo/EXSM3942_Demo/Program.cs
@@ -2,9 +2,41 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int firstNum, secondNum;
+            string ip;
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    ip = await client.GetStringAsync("https://api.ipify.org");
+                }
+            } 
+            catch (Exception e)
+            {
+                ip = "unknown IP address";
+            }
+           
+            Console.WriteLine($"Hello, {Environment.UserName} of {Environment.MachineName} at {ip}!");
+            try
+            {
+                Console.Write("Please enter a number: ");
+                firstNum = int.Parse(Console.ReadLine());
+                Console.Write("Please enter a second number: ");
+                secondNum = int.Parse(Console.ReadLine());
+                RecursiveCount(Math.Min(firstNum, secondNum), Math.Max(firstNum, secondNum));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid Input: "+e.Message);
+            }
+        }
+        static void RecursiveCount(int current, int end)
+        {
+            Console.WriteLine(current);
+            if (current < end) RecursiveCount(current + 1, end);
         }
     }
 }
