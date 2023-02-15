3cf9e7ea8fa18654661ef7752a484d4219f26470
James Grieve <jgrieve@ualberta.ca>
2023-02-14 19:32:40 -0700

    CRUD;

--- src/EXSM3942_Demo/EXSM3942_Demo/Program.cs
+++ src/EXSM3942_Demo/EXSM3942_Demo/Program.cs
@@ -23,25 +23,121 @@ namespace EXSM3942_Demo
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void CreateProduct(string name, string description, int categoryID)
+        {
+            using(GroceryStoreContext context = new GroceryStoreContext())
+            {
+                context.Products.Add(new Product() { Name = name, Description = description, CategoryID = categoryID });
+                context.SaveChanges();
+            }
+        }
+        public static void UpdateProduct(int id, string name, string description, int categoryID)
         {
             using (GroceryStoreContext context = new GroceryStoreContext())
             {
-                string catName, catDesc, prodName, prodDesc;
-                Console.Write("Please enter a category name: ");
-                catName = Console.ReadLine().Trim();
-                Console.Write("Please enter a category description: ");
-                catDesc = Console.ReadLine().Trim();
-                Console.Write("Please enter a product name: ");
-                prodName = Console.ReadLine().Trim();
-                Console.Write("Please enter a product description: ");
-                prodDesc = Console.ReadLine().Trim();
-
-                ProductCategory newCat = new ProductCategory() { Name = catName, Description = catDesc };
-                context.ProductCategories.Add(newCat);
-                context.Products.Add(new Product() { Name = prodName, Description = prodDesc, ProductCategory = newCat });
+                Product toUpdate = context.Products.Where(x => x.ID == id).Single();
+                toUpdate.Name = name;
+                toUpdate.Description = description;
+                toUpdate.CategoryID = categoryID;
                 context.SaveChanges();
             }
+        }
+        public static void DeleteProduct(int id)
+        {
+            using (GroceryStoreContext context = new GroceryStoreContext())
+            {
+                Product toDelete = context.Products.Where(x => x.ID == id).Single();
+                context.Products.Remove(toDelete);
+                context.SaveChanges();
+            }
+        }
+        public static List<Product> GetProducts()
+        {
+            List<Product> products;
+            using (GroceryStoreContext context = new GroceryStoreContext())
+            {
+                products = context.Products.ToList();
+            }
+            return products;
+        }
+        public static int GetCategoryIDByName(string name)
+        {
+            int categoryID;
+            using (GroceryStoreContext context = new GroceryStoreContext())
+            {
+                categoryID = context.ProductCategories.Where(x=>x.Name == name).Single().ID;
+            }
+            return categoryID;
+        }
+        public static int GetProductIDByName(string name)
+        {
+            int categoryID;
+            using (GroceryStoreContext context = new GroceryStoreContext())
+            {
+                categoryID = context.Products.Where(x => x.Name == name).Single().ID;
+            }
+            return categoryID;
+        }
+        static void Main(string[] args)
+        {
+            string input;
+            do
+            {
+                Console.WriteLine("Welcome to the Grocery Store!\n1. Create Product\n2. List Products\n3. Update Product\n4. Delete Product\n0. Exit");
+                Console.Write("\tPlease make a selection: ");
+                input = Console.ReadLine().Trim();
+                if (input == "1")
+                {
+                    string catName, prodName, prodDesc;
+                    int catID;
+                    Console.Write("Please enter a product name: ");
+                    prodName = Console.ReadLine().Trim();
+                    Console.Write("Please enter a product description: ");
+                    prodDesc = Console.ReadLine().Trim();
+                    Console.Write("Please enter a category name: ");
+                    catName = Console.ReadLine().Trim();
+                    catID = GetCategoryIDByName(catName);
+                    CreateProduct(prodName, prodDesc, catID);
+                }
+                else if (input == "2")
+                {
+                    foreach(Product product in GetProducts())
+                    {
+                        Console.WriteLine($"{product.ID} - {product.Name}, {product.Description}");
+                    }
+                }
+                else if (input == "3")
+                {
+                    string targetName, catName, prodName, prodDesc;
+                    int catID, targetID;
+                    Console.Write("Please enter a current product name: ");
+                    targetName = Console.ReadLine().Trim();
+                    targetID = GetProductIDByName(targetName);
+
+                    Console.Write("Please enter the updated product name: ");
+                    prodName = Console.ReadLine().Trim();
+                    Console.Write("Please enter the updated product description: ");
+                    prodDesc = Console.ReadLine().Trim();
+                    Console.Write("Please enter the updated category name: ");
+                    catName = Console.ReadLine().Trim();
+                    catID = GetCategoryIDByName(catName);
+                    UpdateProduct(targetID, prodName, prodDesc, catID);
+                }
+                else if (input == "4")
+                {
+                    string targetName;
+                    int targetID;
+                    Console.Write("Please enter a current product name: ");
+                    targetName = Console.ReadLine().Trim();
+                    targetID = GetProductIDByName(targetName);
+                    DeleteProduct(targetID);
+                }
+                else if (input != "0")
+                {
+                    Console.WriteLine("Error, please make a valid selection.");
+                }
+                Console.WriteLine();
+            } while (input != "0");
         }
     }
 }
